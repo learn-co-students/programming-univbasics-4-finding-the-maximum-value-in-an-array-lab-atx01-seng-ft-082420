5f56fa587459b79d074666ba4d31c21a77630573
Felipe Dolago <lagofelipe@gmail.com>
2020-07-27 14:15:29 +0000

    Done.

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,9 +1,11 @@
-def find_element_index(array, value_to_find)
+def find_max_value(array)
 count = 0
+maximum_value = -1
 while count < array.length do
-  if array[count] == value_to_find
-    return count
-  end
+  if array[count] > maximum_value
+    maximum_value = array[count]
+    end
 count += 1
 end
+return maximum_value
 end
